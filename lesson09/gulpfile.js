15df1ebbb12acc417df270e3d5aa9faf01be4fcd
George <george.yevtushenko@youshido.com>
2017-01-13 13:37:23 +0200

    lesson09: add connect

--- lesson09/gulpfile.js
+++ lesson09/gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require('gulp');
 var concatCss = require('gulp-concat-css');
 var livereload = require('gulp-livereload');
+var connect = require('gulp-connect');
 
-//gulp.task('default', function() {
-//    // place code for your default task here
-//});
+gulp.task('default', function() {
+    // place code for your default task here
+});
 
 
-//gulp.task('default', function() {
+//gulp.task('css', function() {
 //    gulp.src('css/*.css')
 //        .pipe(concatCss('bundle.css'))
 //        .pipe(gulp.dest('web/'))
@@ -15,5 +16,33 @@ var livereload = require('gulp-livereload');
 //});
 //
 //gulp.task('watch', function() {
-//    gulp.watch('css/*.css', ['default']);
+//    gulp.watch('css/*.css', ['css']);
 //});
+//
+//gulp.task('default', ['css', 'watch']);
+
+
+
+
+
+
+//gulp.task('connect', function() {
+//    connect.server({
+//        root: 'web',
+//        livereload: true
+//    });
+//});
+//
+//gulp.task('css', function() {
+//    gulp.src('css/*.css')
+//        .pipe(concatCss('bundle.css'))
+//        .pipe(gulp.dest('web/'))
+//        .pipe(connect.reload());
+//});
+//
+//gulp.task('watch', function() {
+//    gulp.watch('css/*.css', ['css']);
+//});
+//
+//gulp.task('default', ['connect', 'css', 'watch']);
+

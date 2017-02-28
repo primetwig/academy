d73ce1506ce36722d89a34e23a71161c5987c223
George <george.yevtushenko@youshido.com>
2017-02-28 12:19:37 +0200

    update lesson18

--- lesson18/src/main.js
+++ lesson18/src/main.js
@@ -1,1 +1,68 @@
 var app = angular.module('app', []);
+
+//app.controller('StoreController', function ($scope) {
+//    this.product = {
+//        name: "Emerald",
+//        price: 5.30,
+//        description: "Some text goes here"
+//    };
+//
+//    $scope.merchant = {
+//        name: "Dealer name"
+//    };
+//});
+
+
+//app.controller('StoreController', function () {
+//    this.product = {
+//        name: "Emerald",
+//        price: 5.30,
+//        description: "Some text goes here",
+//        canPurchase: true,
+//        soldOut: true
+//    };
+//});
+
+
+app.controller('StoreController', function () {
+    this.products = [
+        {
+            name: "Emerald",
+            price: 5.30,
+            description: "Some text goes here",
+            canPurchase: true
+        },
+        {
+            name: "Sapphire",
+            price: 12.70,
+            description: "Some text goes here as well",
+            canPurchase: false
+        }
+    ];
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

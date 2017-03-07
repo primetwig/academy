1bc83f1167d9cc934edff8ce627744363c8980ab
George <george.yevtushenko@youshido.com>
2017-03-08 01:27:10 +0200

    update lesson20

--- lesson20/src/main.js
+++ lesson20/src/main.js
@@ -50,64 +50,93 @@ var app = angular.module('app', []);
 
 
 app.controller('StoreController', function () {
-    this.products = (new Array(1))
-        .join('a')
-        .split('a')
-        .map(v => {
-            return {
-                name:        random.name(),
-                price:       random.price(),
-                description: random.sentence(),
-                canPurchase: random.boolean(),
-                soldOut:     random.boolean(),
-                image:       random.image(),
-                images:      random.images(),
-                reviews:     [{
-                    stars:  random.stars(),
-                    body:   random.sentence(),
-                    author: random.email()
-                }, {
-                    stars:  random.stars(),
-                    body:   random.sentence(),
-                    author: random.email()
-                }, {
-                    stars:  random.stars(),
-                    body:   random.sentence(),
-                    author: random.email()
-                }]
-            };
-        });
+
+    this.numbers = [1, 2, 3, 4, 5];
 
     this.colors = [
         { id: 1, name:'black',  shade:'dark' },
         { id: 2, name:'white',  shade:'light' },
         { id: 3, name:'red',    shade:'dark' },
-        { id: 4, name:'blue',   shade:'dark' },
+        { id: 4, name:'blue',   shade:'dark', disabled: true },
         { id: 5, name:'yellow', shade:'light' }
     ];
 
-});
+    //this.color1 = 2;
+    //this.color2 = 2;
+    //this.color3 = 2;
+    //this.color4 = 2;
 
-app.controller('PanelController', function () {
-    this.tab = 3;
-
-    this.selectTab = function (tab) {
-        this.tab = tab;
-    };
-
-    this.isSelected = function (tab) {
-        return this.tab == tab;
-    };
+    //this.color1 = this.colors[2];
+    //this.color2 = this.colors[2];
+    //this.color3 = this.colors[2];
+    //this.color4 = this.colors[2];
 
 });
 
-app.controller('GalleryController', function () {
-    this.current = 0;
 
-    this.setCurrent = function(index){
-        this.current = index || 0;
-    };
-});
+
+
+
+
+//app.controller('StoreController', function () {
+//    this.products = (new Array(1))
+//        .join('a')
+//        .split('a')
+//        .map(v => {
+//            return {
+//                name:        random.name(),
+//                price:       random.price(),
+//                description: random.sentence(),
+//                canPurchase: random.boolean(),
+//                soldOut:     random.boolean(),
+//                image:       random.image(),
+//                images:      random.images(),
+//                reviews:     [{
+//                    stars:  random.stars(),
+//                    body:   random.sentence(),
+//                    author: random.email()
+//                }, {
+//                    stars:  random.stars(),
+//                    body:   random.sentence(),
+//                    author: random.email()
+//                }, {
+//                    stars:  random.stars(),
+//                    body:   random.sentence(),
+//                    author: random.email()
+//                }]
+//            };
+//        });
+//});
+//
+//app.controller('PanelController', function () {
+//    this.tab = 3;
+//
+//    this.selectTab = function (tab) {
+//        this.tab = tab;
+//    };
+//
+//    this.isSelected = function (tab) {
+//        return this.tab == tab;
+//    };
+//
+//});
+//
+//app.controller('GalleryController', function () {
+//    this.current = 0;
+//
+//    this.setCurrent = function(index){
+//        this.current = index || 0;
+//    };
+//});
+//
+//app.controller('ReviewController', function () {
+//    this.review = {};
+//
+//    this.addReview = function(product){
+//        product.reviews.push(this.review);
+//        this.review = {};
+//    };
+//});
 
 
 

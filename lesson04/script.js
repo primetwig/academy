614c1d68635c0fc30aebfc378627b39f615d741d
George <george.yevtushenko@youshido.com>
2017-06-06 16:22:25 +0300

    update

--- lesson04/script.js
+++ lesson04/script.js
@@ -1,50 +1,10 @@
-function log(str) {
-    console.log('num: '+str);
-}
-function scream(str) {
-    console.log('num: '+str+'!!!!!!');
-}
 
-function doSomeMath(callback) {
-    var a = 14;
-    var b = 15;
+var btn = document.querySelector('.btn');
 
-    var c = a + b;
+btn.addEventListener('click', e => console.log('click 1'));
 
-    callback(c);
-}
+btn.addEventListener('click', e => console.log('click 1'), true);
 
-doSomeMath(log);
-doSomeMath(scream);
+btn.onclick = e => console.log('click');
 
-
-
-
-
-var names, total, sorted;
-var users = [
-    { name: "Alex", cash: 43 },
-    { name: "John", cash: 274 },
-    { name: "Carl", cash: 82 },
-    { name: "Bill", cash: 14 }
-];
-
-names = users.map(function (user) {
-    return user.name;
-});
-
-total = users.reduce(function (sum, user) {
-    return sum + user.cash;
-}, 0);
-
-sorted = users.slice(0).sort(function (user1, user2) {
-    return user1.cash - user2.cash;
-}, 0);
-
-
-
-console.log(names);
-console.log(total);
-console.log(sorted);
-console.log(users);
 

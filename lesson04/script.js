4cec12bf537508a846f2748b217c1f16c0d0bfb3
George <george.yevtushenko@youshido.com>
2017-06-06 16:23:03 +0300

    update

--- lesson04/script.js
+++ lesson04/script.js
@@ -1,10 +1,10 @@
-
+var d = document;
 var btn = document.querySelector('.btn');
 
 btn.addEventListener('click', e => console.log('click 1'));
 
-btn.addEventListener('click', e => console.log('click 1'), true);
+btn.addEventListener('click', e => console.log('click 2'), true);
 
-btn.onclick = e => console.log('click');
+btn.onclick = e => console.log('click 3');
 
 
